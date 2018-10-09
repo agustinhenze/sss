427147fedb92822f8cfbc60a45ed8e3ca8378c81
Agustin Henze <tin@redhat.com>
2018-10-09 15:07:07 +0200

    Add test_get_build_metadata

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,3 +29,40 @@ class TestMain(unittest.TestCase):
             {"basehead": "githash", "baserepo": "url"},
             result,
         )
+
+    def test_get_build_metadata(self):
+        data = {}
+        arch = 'x86_64'
+        self.assertRaises(sss.MissingField, sss.get_build_metadata, data, arch)
+        data = {
+            'cfgurl': 'url',
+            'buildurl': 'url',
+            'foo': 'bar'
+        }
+        result = sss.get_build_metadata(data, arch)
+        self.assertDictEqual(
+            {'buildurl_x86_64': 'url', 'cfgurl_x86_64': 'url'},
+            result,
+        )
+
+        data = {
+            'cfgurl': 'url',
+            'buildlog': 'url',
+            'foo': 'bar'
+        }
+        result = sss.get_build_metadata(data, arch)
+        self.assertDictEqual(
+            {'buildlog_x86_64': 'url', 'cfgurl_x86_64': 'url'},
+            result,
+        )
+
+        data = {
+            'basehead': 'githash',
+            'cfgurl': 'url',
+            'foo': 'bar'
+        }
+        result = sss.get_build_metadata(data, arch, check_missing_fields=False)
+        self.assertDictEqual(
+            {"cfgurl_x86_64": "url"},
+            result,
+        )
